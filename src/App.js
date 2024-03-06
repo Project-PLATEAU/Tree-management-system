031aed5d114f53c46ebb4acb0ec50f185e73fb52
yasoshima <yutaka.yasoshima@pacificspatial.com>
2024-03-06 14:22:04 +0900

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import './App.css'
-import {BrowserRouter, Route, Routes} from "react-router-dom"
+import React, { useEffect, useMemo, useState } from "react"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Root from "./views/root"
-import "ag-grid-enterprise"
-import "ag-grid-community/styles/ag-grid.css"
-import "ag-grid-community/styles/ag-theme-balham.css"
-import { LicenseManager } from "ag-grid-enterprise"
-import {useEffect} from "react"
+import Map from "./views/root/map"
+import UseAuthData from "./auth"
+import LoginView from "./views/login"
+import "firebase/compat/auth"
+import { getAuth, onAuthStateChanged } from "firebase/auth"
+import { Box, CircularProgress } from "@mui/material"
+import { makeStyles } from "@mui/styles"
+import "dayjs/locale/ja"
+import dayjs from "dayjs"
+import {First} from "./manager/carto";
+export const AuthDataContext = React.createContext()
 
-function App() {
+dayjs.locale("ja")
 
-    useEffect(() => {
-        process.env.REACT_APP_AG_GRID_LICENSE_KEY && LicenseManager.setLicenseKey(process.env.REACT_APP_AG_GRID_LICENSE_KEY)
-    }, []);
+const useStyles = makeStyles({
+  circle: {
+    width: "100vw",
+    height: "100vh",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    overflow: "hidden",
+    backgroundColor: "#333",
+  },
+})
+
+const App = () => {
+  const classes = useStyles()
+  const useAuthData = UseAuthData()
+  const { authState, setAuth, login } = useAuthData
+  const [authInitialized, setAuthInitialized] = useState(false)
+
+  useMemo(() => {
+    return setAuth(getAuth())
+  }, [])
+
+  useEffect(() => {
+    if (!authState.auth) {
+      return
+    }
+    onAuthStateChanged(authState.auth, (user) => {
+      if (user && user.email) {
+        First(
+            `SELECT * FROM ${process.env.REACT_APP_TABLE_USER} WHERE LOWER(email) = '${user.email.toLowerCase()}'`
+        )
+            .then((res) => {
+              console.log('[Login]', 'user', res)
+              login({ ...user, detail: res })
+            })
+            .catch((e) => {
+              login({ ...user, detail: {} })
+            })
+      } else if (user) {
+        login({ ...user, detail: {} })
+      } else {
+        login(user)
+      }
+
+      setAuthInitialized(true)
+    })
+  }, [authState.auth])
+
 
   return (
-      <BrowserRouter>
-        <Routes>
-          <Route exact path="/" element={<Root />} />
-        </Routes>
-      </BrowserRouter>
-  );
+    <AuthDataContext.Provider value={useAuthData}>
+      {!authInitialized && (
+        <Box className={classes.circle}>
+          <CircularProgress color="success" />
+        </Box>
+      )}
+      {authInitialized && !authState.userData && <LoginView />}
+      {authInitialized && authState.userData && (
+        <BrowserRouter>
+          <Routes>
+            <Route exact path="/" element={<Root />} />
+            <Route path="/map" element={<Map />} />
+          </Routes>
+        </BrowserRouter>
+      )}
+    </AuthDataContext.Provider>
+  )
 }
 
-export default App;
+export default App
